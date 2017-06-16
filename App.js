3b8eba64ceb73d6c00fd469d5ed5617fc8cf8d33
Nikita Korotkih <nkor@hellofresh.com>
2017-06-16 15:01:45 +0200

    feat(*): doit button

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, Image } from 'react-native';
+import Svg, { Circle } from 'react-native-svg';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (<Image source={require('./img/norris.jpg')}
+             style={styles.container}
+            >
+            <Svg
+                height="350"
+                width="300"
+            >
+                <Circle
+                    cx="150"
+                    cy="250"
+                    r="100"
+                    stroke="black"
+                    strokeWidth="3.5"
+                    fill="green"
+                    onPress={() => Alert.alert('hi there')}
+                />
+                <Circle
+                    cx="150"
+                    cy="250"
+                    r="50"
+                    stroke="yellow"
+                    strokeWidth="4.5"
+                    fill="red"
+                    onPress={() => Alert.alert('hi there')}
+                />
+            </Svg>
+        </Image>);
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        resizeMode: 'cover',
+        width: '100%',
+    },
 });
